ebf8b5207679ff4ab7155196ffd0c82d77d4cf21
ShubhamSonawane007 <sksonawane34@gmail.com>
2025-02-14 15:04:56 +0530

    api changes

---
+++ app/api/send/route.js
@@ -0,0 +1,28 @@
+import { EmailTemplate } from "@/components/EmailTemplate/email-template";
+import { NextResponse } from "next/server";
+import { Resend } from "resend";
+
+const resend = new Resend(process.env.RESEND_API_KEY);
+
+export async function POST(request) {
+    try {
+        const body = await request.json();
+        console.log("body", body);
+        const { email, name, subject, message } = body;
+        const data = await resend.emails.send({
+            from: "mahendrasutar811@gmail.com",
+            to: email,
+            reply_to: "mahendrasutar811@gmail.com",
+            cc: "mahendrasutar811@gmail.com",
+            subject: subject,
+            react: EmailTemplate({ senderName: name, messageBody: message }),
+        });
+
+        if (data.status === "success") {
+            return NextResponse.json({ message: "Email Successfully Sent!" });
+        }
+        return NextResponse.json(data);
+    } catch (error) {
+        console.log("error", error);
+    }
+}
